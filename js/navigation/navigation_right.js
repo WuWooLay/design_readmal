f75ce0153a392bce406d700c0ac0269c8ae7d678
WuWooLay <mamamglay.lp@gmail.com>
2018-12-11 04:25:16 +0630

    Right Navigation Added

--- js/navigation/navigation_right.js
+++ js/navigation/navigation_right.js
@@ -2,7 +2,26 @@ $(document).ready( function () {
 
     // When Open
     $('#Navigation_Right_Opener').click( function () {
-        $('#Navigation_Right').css('display','block');
+        
+        var cssProperties = anime({
+            targets: '#Navigation_Right',
+            left: '0',
+            easing: 'easeInOutQuad',
+            duration: 500
+        });
+
+
     });
 
+    // When Close
+    $('#Navigation_Right_Closer').click( function () {
+        var cssProperties = anime({
+            targets: '#Navigation_Right',
+            left: '100%',
+            easing: 'easeInOutQuad',
+            duration: 500
+        });
+    });
+
+    
 });
