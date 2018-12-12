8971ac2b708fb17d1e30e537118a0e1a854e583d
WuWooLay <mamamglay.lp@gmail.com>
2018-12-13 01:10:10 +0630

    Add Music Mp3 Js And Make Wallper Change

--- js/main_section/main_section.js
+++ js/main_section/main_section.js
@@ -1,15 +1,5 @@
 $(document).ready( function () {
 
-    $('.More a').click( function () {
-        $('#Loading_Container').removeClass('d-none');
-
-        setTimeout(function () {
-            $('#Loading_Container').addClass('d-none');
-            $('#Story_Model').removeClass('d-none');
-        }, 2500);
-    });
-
-
     $('#Back').click( function () {
         $('#Story_Model').addClass('d-none');
     });
--- js/main_section/story_model.js
+++ js/main_section/story_model.js
@@ -9,6 +9,11 @@ $(document).ready( function () {
         alert('Your Web Browser not supported Vibrate');
     }
 
+    var audioPlay = function (Audio_Url) {
+        var audio = new Audio(Audio_Url);
+        audio.play();
+    };
+
     var buildNormalMessage = function (Message_Position, Message_Color, Message, Message_Teller) {
         var c = 'Message_'+ Message_Position;
         var word = '<li class="Message_Story '+ c +' zoomIn animated" data-color="" >';
@@ -42,15 +47,23 @@ $(document).ready( function () {
     };
 
     var messageArray = [
-        {id: '0', name: 'Lwin', position: 'Center', type: 'think', message: 'တကယ္ေတာ့......', color: '#43a047', vibrate: true, vibrate_pattern: [800], background_change: false, background_image: ''},
-        {id: '1', name: 'ေကာင္းထက္', position: 'Left', type: 'message', message: 'ဘာမွမျဖစ္ေလာက္ပါဘူးကြာ', color: '#43a047', vibrate: true, vibrate_pattern: [800], background_change: false, background_image: ''},
-        {id: '2', name: 'ေက်ာ္ေက်ာ္', position: 'Left', type: 'message', message: 'ေအးဆုိ', color: '#0b93f6', vibrate: false, vibrate_pattern: '', background_change: false, background_image: ''},
-        {id: '3', name: 'ေက်ာ္ေက်ာ္', position: 'Left', type: 'message', message: 'ခုခ်ိန္ဆုိ ဘယ္သူ့မွလည္းရွိမွာမဟုတ္သးဘူး', color: '#0b93f6', vibrate: true, vibrate_pattern: [500,200,500,800,500,200,500], background_change: true, background_image: '/images/wall_paper/Evil_With_Sword_in_Dark.jpg'},
-        {id: '4', name: 'ေက်ာ္ေက်ာ္', position: 'Left', type: 'message', message: 'ေတြ့လားသားေလး', color: '#0b93f6', vibrate: false, vibrate_pattern: '', background_change: false, background_image: ''},
+        {id: '1', name: 'ေကာင္းထက္', position: 'Left', type: 'message', message: 'ဘာမွမျဖစ္ေလာက္ပါဘူးကြာ', color: '#2cc34e', vibrate: true, vibrate_pattern: [800], background_change: false, background_image: '',  audio: true, audio_url: '/audio/ghost/ghost_bell.mp3'},
+        {id: '2', name: 'ေက်ာ္ေက်ာ္', position: 'Left', type: 'message', message: 'ေအးဆုိ', color: '#0b93f6', vibrate: false, vibrate_pattern: '', background_change: false, background_image: '', audio: false, audio_url: ''},
+        {id: '3', name: 'ေက်ာ္ေက်ာ္', position: 'Left', type: 'message', message: 'ခုခ်ိန္ဆုိ ဘယ္သူ့မွလည္းရွိမွာမဟုတ္သးဘူး', color: '#0b93f6', vibrate: true, vibrate_pattern: [500,200,500,800,500,200,500], background_change: false, background_image: '', audio: false, audio_url: ''},
+        {id: '4', name: 'ေက်ာ္ေက်ာ္', position: 'Left', type: 'message', message: 'ေတြ့လားသားေလး', color: '#0b93f6', vibrate: false, vibrate_pattern: '', background_change: true, background_image: '/images/story_card/ghost_door.jpg', audio: true, audio_url: '/audio/ghost/creaking_door.mp3'},
+        {id: '5', name: 'ႏွင္းႏွင္း', position: 'Right', type: 'message', message: 'နင္တုိ့ကလည္းဟာ ဘာလုိ့ တံခါးအရင္မေခါက္တာလဲ..', color: '#dc3545', vibrate: false, vibrate_pattern: '', background_change: false, background_image: '', audio: false, audio_url: ''},
+        {id: '6', name: ' ', position: 'Center', type: 'think', message: 'ဒီလိုညၾကီးမွာ ကားပ်က္သြားတဲ့ သူငယ္ခ်င္းေတြအဖုိ့ေတာ့ ......  ', color: '#43a047', vibrate: true, vibrate_pattern: [800], background_change: false, background_image: '', audio: false, audio_url: ''},
+        {id: '7', name: 'ေက်ာ္ေက်ာ္', position: 'Left', type: 'message', message: 'အာ့ေၾကာင့္ ငါေျပာသားပဲ မင္းတုိ့ အတြဲသာ ညဘက္ၾကီး ခရီးသြားဖို့ မေျပာခဲ့ရင္ ဘာမွျဖစ္မွာမဟုတ္ဘူး', color: '#0b93f6', vibrate: false, vibrate_pattern: '', background_change: false, background_image: '', audio: false, audio_url: ''},
+        {id: '8', name: 'ႏွင္းႏွင္း', position: 'Right', type: 'message', message: ' ရွဴး တုိးတုိးလုပ္ပါဟာ တျခားသူေတြ ၾကားရင္.. ငါေၾကာက္တယ္ဟ ', color: '#dc3545', vibrate: false, vibrate_pattern: '', background_change: false, background_image: '', audio: false, audio_url: ''},
+    ];
+
+    var audio_list = [
+
     ];
 
     var count = 0;
 
+    console.log(messageArray);
     
     $('#Story_Model_Content_Message_Section').click( function () {
        
@@ -60,6 +73,12 @@ $(document).ready( function () {
         }
 
         var build = '';
+
+
+        if(messageArray[count].audio) {
+            audioPlay(messageArray[count].audio_url);
+        }
+
         
         if(messageArray[count].type == 'message') {
             build = buildMessage(messageArray[count].position, messageArray[count].color, messageArray[count].message , messageArray[count].name  );
@@ -69,10 +88,9 @@ $(document).ready( function () {
             alert('The Error');
             return false;
         }
-
-        
-        
         $('#Message_Story_List').append(build);
+
+
 
         $('#Story_Model_Content_Message_Section').stop().animate(
         {
@@ -91,6 +109,37 @@ $(document).ready( function () {
         count++;
     });
 
+
+    // No Need
+
+    $('.More a').click( function () {
+        $('#Loading_Container').removeClass('d-none');
+
+        count = 0;
+
+        setTimeout(function () {
+            $('#Loading_Container').addClass('d-none');
+            $('#Story_Model').removeClass('d-none');
+
+            $('#Message_Story_List > li').remove();
+
+            if(messageArray[count].audio) {
+                audioPlay(messageArray[count].audio_url);
+            }
+
+            if(messageArray[count].type == 'message') {
+                build = buildMessage(messageArray[count].position, messageArray[count].color, messageArray[count].message , messageArray[count].name  );
+            } else if (messageArray[count].type == 'think') {
+                build = buildThink(messageArray[count].position, messageArray[count].color, messageArray[count].message , messageArray[count].name  );
+            } else {
+                alert('The Error');
+                return false;
+            }
+            
+            $('#Message_Story_List').append(build);
+            count++;
+        }, 1500);
+    });
     
     
 });
